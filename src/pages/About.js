1cb95f42896d6e9c6441e0c2f9034487d3eb1edf
kryptokessler <51162656+kryptokessler@users.noreply.github.com>
2023-08-20 15:36:20 +0200

    Includes BrowserRouter

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <div className="home">
+            <p>
+                <h1>MAKE Europe: Consulting for the Internet of Value #Web3</h1>
+            </p>
+            <p>
+            Developing software Made in Germany since 2015, the Make Europe GmbH is among the leading pioneers in the fields DLT, Blockchain, EVM, Metaverse-Technology, Web3 as well as Online Payments and Online Brokerage.
+     
+            </p>
+        </div>
+    )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+export default function Home() {
+    return (
+        <div className="home">
+            <p>
+                <h1>MAKE Europe: Consulting for the Internet of Value #Web3</h1>
+            </p>
+            <p>
+                <h2>Mythbusgting lessons</h2>
+                <li>Let's talk about Bitcoin and Lightning.</li>
+                <li>Let's talk about Layer-2 Blockchains?</li>
+                <li>Let'sheck, if you ready for the Digital €uro?</li>
+                <li>Talk to us aout the #Metaverse?</li>
+     
+            </p>
+        </div>
+    )
+}
